60244f74b891eaba040109e5eb4d282bd55fb20b
Kellen <bolger.kellen@gmail.com>
2024-10-18 00:11:40 -0400

    chore: add integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://bluelinedigital.io',
+  integrations: [react(), tailwind(), sitemap(), robotsTxt(), icon()],
+});
